66ba1c0a06a2a90e57ce125bc98900e8357cf300
Do Thien Hoang <theksbd@gmail.com>
2023-01-19 17:52:38 +0700

    improve performance

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-// https://v2.jokeapi.dev/joke/Dark?blacklistFlags=racist
-let joke;
 const apiURL =
   "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
-const fetchData = (apiURL) => {
-  fetch(apiURL)
-    .then((res) => res.json())
-    .then((data) => (joke = data))
-    .catch((err) => console.log(err));
+const fetchData = async (apiURL) => {
+  try {
+    const res = await fetch(apiURL);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const getJoke = () => {
-  if (joke.type === "single") {
-    document.querySelector(".part-1").innerHTML = "";
-    document.querySelector(".part-2").innerHTML = joke.joke;
-  } else {
-    document.querySelector(".part-1").innerHTML = joke.setup;
-    document.querySelector(".part-2").innerHTML = joke.delivery;
-  }
+const getJoke = async () => {
+  const joke = await fetchData(apiURL);
+
+  document.querySelector(".part-1").innerHTML =
+    joke.type === "single" ? "" : joke.setup;
+  document.querySelector(".part-2").innerHTML =
+    joke.type === "single" ? joke.joke : joke.delivery;
 };
 
 const randomPerson = () => {
   const number = Math.floor(Math.random() * 7);
-  const src = "./assets/" + number + ".png";
+  const src = `./assets/${number}.png`;
   document.getElementById("image").src = src;
 };
 
-fetchData(apiURL);
-setTimeout(getJoke, 500);
+const initialize = async () => {
+  await getJoke(apiURL);
+  randomPerson();
+};
 
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", () => {
-  fetchData(apiURL);
-  setTimeout(() => {
-    randomPerson();
-    getJoke();
-  }, 500);
+initialize();
+
+document.querySelector(".btn").addEventListener("click", () => {
+  initialize();
 });
